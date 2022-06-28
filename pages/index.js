f92e29a0e601fbf198657d8288c121c9c1fb3887
Mauricio Robinson <marsjr97@gmail.com>
2022-06-28 13:07:55 -0400

    deleting console logs

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,6 @@ import Navbar from "./components/Navbar";
 
 export default function Home() {
   const [darkMode, setDarkMode] = useState(false);
-  console.log(darkMode);
 
   function handleToggleDarkMode() {
     setDarkMode((prevDarkMode) => !prevDarkMode);

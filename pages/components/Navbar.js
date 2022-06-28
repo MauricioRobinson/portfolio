ab08d55d94f0482f598377e27044a735bb790eb1
Mauricio Robinson <marsjr97@gmail.com>
2022-06-28 00:02:23 -0400

    Navbar initial design

---
+++ pages/components/Navbar.js
@@ -0,0 +1,28 @@
+function Navbar() {
+  return (
+    <div>
+      <nav className="h-16 shadow-md flex justify-between items-center px-4">
+        <div className="font-semibold tracking-wide">
+          <h1>Portfolio</h1>
+        </div>
+        <div className="flex justify-evenly items-center">
+          <ul className="font-semibold tracking-wide">
+            <li className="inline-block mr-4 cursor-pointer">Home</li>
+            <li className="inline-block mr-4 cursor-pointer">Contact</li>
+            <li className="inline-block mr-4 cursor-pointer">Blog</li>
+            <li className="inline-block mr-4 cursor-pointer">Showcase</li>
+          </ul>
+          <div className="flex items-center font-semibold tracking-wide gap-2 text-xs">
+            <span>Light</span>
+            <div className="w-6 h-3 bg-slate-900 rounded-md cursor-pointer">
+              <div className="w-3 h-3 rounded-full bg-white"></div>
+            </div>
+            <span>Dark</span>
+          </div>
+        </div>
+      </nav>
+    </div>
+  );
+}
+
+export default Navbar;
